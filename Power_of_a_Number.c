0bc07d7b9b0603e1b5c6da70f383c730d64726dc
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-04-24 09:01:37 +0530

    #include<stdio.h> #include<math.h> int main() {     int x,y,m,r,d;     scanf("%d%d%d",&x,&y,&m);    {        r=pow(x,y);    }
       {
       d=r%m;
       }
       printf("%d",d);
    }

---
+++ Power_of_a_Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,y,m,r,d;
+    scanf("%d%d%d",&x,&y,&m);
+   {
+       r=pow(x,y);
+   }
+   
+   {
+   d=r%m;
+   }
+   printf("%d",d);
+}
